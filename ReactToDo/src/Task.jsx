8a0507883322dc2c257fceab445c5cc7b383e9b4
Onyeabor Joel <earlmorningstar@gmail.com>
2024-01-23 14:43:21 +0100

    Update Task.jsx

--- ReactToDo/src/Task.jsx
+++ ReactToDo/src/Task.jsx
@@ -1,42 +1,28 @@
 export default function Task() {
-    return (
-        <>
-        <div className="layoutBox">
-            <div className="layoutMin">
-                <div className="checknTitle">
-                    <input className="checkbox" type="checkbox"/>
-                    <div>
-                        <div className="taskTitle">Create A  React Project</div>
-                        <div className="timenDate">
-                            <span>Time,</span>
-                            <span>Date</span>
-                        </div>
-                    </div>
-                </div>
-                <div className="screenBtn">
-                    <button>Delete</button>
-                    <button>Edit</button>
-                </div>
+
+    
+  return (
+    <>
+      <div className="layoutBox">
+        <div className="layoutMin">
+          <div className="checknTitle">
+            <input className="checkbox" type="checkbox" />
+            <div>
+              <div className="taskTitle">Create A React Project</div>
+              <div className="timenDate">
+                <span>Time,</span>
+                <span>Date</span>
+              </div>
             </div>
-           
-            
+          </div>
+          <div className="screenBtn">
+            <button>Delete</button>
+            <button>Edit</button>
+          </div>
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
 
 
-{/* <div className="item">
-<input type="checkbox" />
-<div>
-    <p>Create a React Project</p>
-    <span className="datenTime">
-        <p>Time</p>
-        <p>Date</p>
-    </span>
-</div>
-<div>
-    <span>Delete</span>
-    <span>Edit</span>
-</div>
-</div> */}
